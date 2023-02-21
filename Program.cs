fdf59798f20d8f1b6b2fb7326ec240493cef3d69
Huletski Alexander <alexander.huletski@gmail.com>
2023-02-21 16:22:31 +0300

    was added task3

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿ //task3 Написать программу копирования массива
+ 
+ static void Copy()
+ {
+        int[] array = {1, 3, 9, 23, 11};
+
+        Console.Write("Original array: ");
+        Console.WriteLine(String.Join(", ", array));
+        
+        int[] copy = new int[array.Length];
+        array.CopyTo(copy, 0);
+
+        Console.Write("Copy of array:  ");
+        Console.WriteLine(String.Join(", ", copy));
+ }
+
+ Copy();
